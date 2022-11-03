e052d1390b0f0ee5ce91646ce7497c378180dc2f
othaime-en <61498231+othaime-en@users.noreply.github.com>
2022-11-03 09:00:48 +0000

    add deploy script

---
+++ deploy/01-deploy-basic-nft.js
@@ -0,0 +1,24 @@
+const { network, ethers } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    log("###################################")
+    const args = []
+    const basicNft = await deploy("BasicNft", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmation: network.config.blockConfirmations || 1,
+    })
+    console.log("######----BasicNFT contract deployed----########")
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        await verify(basicNft.address, args)
+    }
+}
+
+module.exports.tags = ["all", "BasicNft"]
