8416eab7b5a01dd6d5f474b9a81b2ee188e40bed
othaime-en <61498231+othaime-en@users.noreply.github.com>
2022-11-07 10:40:47 +0000

    added subscriptionId to hardhat network

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -2,6 +2,7 @@ const networkConfig = {
     31337: {
         name: "localhost",
         ethUsdPriceFeed: "0x9326BFA02ADD2366b30bacB125260Af641031331",
+        subscriptionId: "6107",
         gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
         mintFee: "10000000000000000", // 0.01 ETH
         callbackGasLimit: "500000", // 500,000 gas
