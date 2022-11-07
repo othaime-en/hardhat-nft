1d14856bdcae8cbb3cfa884c7e97764c257087ff
othaime-en <61498231+othaime-en@users.noreply.github.com>
2022-11-07 10:31:47 +0000

    added uploadToPinata file

---
+++ utils/uploadToPinata.js
@@ -0,0 +1,43 @@
+const pinataSDK = require("@pinata/sdk")
+const path = require("path")
+const fs = require("fs")
+require("dotenv").config()
+
+const pinataApiKey = process.env.PINATA_API_KEY
+const pinataSecretApiKey = process.env.PINATA_API_SECRET
+const pinata = pinataSDK(pinataApiKey, pinataSecretApiKey)
+
+async function storeImages(imagesFilePath) {
+    const fullImagesFilePath = path.resolve(imagesFilePath)
+    const files = fs.readdirSync(fullImagesFilePath)
+    let responses = []
+    console.log("Uploading images to Pinata...")
+
+    for (fileIndex in files) {
+        console.log(`Uploading ${files[fileIndex]} to Pinata...`)
+        // creating a stream to stream all the data in the image files
+        // This is a requirement to using the pinata SDK
+        const readableStreamForFile = fs.createReadStream(
+            `${fullImagesFilePath}/${files[fileIndex]}`
+        )
+        try {
+            const response = await pinata.pinFileToIPFS(readableStreamForFile)
+            responses.push(response)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+    return { responses, files }
+}
+
+async function storeTokenUriMetadata(tokenUriMetadata) {
+    try {
+        const response = await pinata.pinJSONToIPFS(tokenUriMetadata)
+        return response
+    } catch (error) {
+        console.log(error)
+    }
+    return null
+}
+
+module.exports = { storeImages, storeTokenUriMetadata }
