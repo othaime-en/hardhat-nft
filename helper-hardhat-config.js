0623a128bfda2ec6ccda8de843b2cd79f3d0ef6a
othaime-en <61498231+othaime-en@users.noreply.github.com>
2022-11-07 10:42:53 +0000

    improved readability

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -2,10 +2,10 @@ const networkConfig = {
     31337: {
         name: "localhost",
         ethUsdPriceFeed: "0x9326BFA02ADD2366b30bacB125260Af641031331",
-        subscriptionId: "6107",
         gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
-        mintFee: "10000000000000000", // 0.01 ETH
         callbackGasLimit: "500000", // 500,000 gas
+        mintFee: "10000000000000000", // 0.01 ETH
+        subscriptionId: "6107",
     },
     // Price Feed Address, values can be obtained at https://docs.chain.link/docs/reference-contracts
     5: {
