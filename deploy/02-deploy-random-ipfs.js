93ec53460a6d126cdd5ba72ee54dea0e855a319e
othaime-en <61498231+othaime-en@users.noreply.github.com>
2022-11-07 11:18:16 +0000

    spacing

--- deploy/02-deploy-random-ipfs.js
+++ deploy/02-deploy-random-ipfs.js
@@ -4,6 +4,7 @@ const { verify } = require("../utils/verify")
 const { storeImages, storeTokenUriMetadata } = require("../utils/uploadToPinata")
 
 const FUND_AMOUNT = ethers.utils.parseEther("0.1")
+
 const imagesLocation = "./images/randomNft"
 const metadataTemplate = {
     name: "",
