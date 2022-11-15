6b97c187b7b45bb37fc7ece5d3601aa5a2425e49
othaime-en <61498231+othaime-en@users.noreply.github.com>
2022-11-15 11:29:27 +0000

    added dynamicSvg deploy script

---
+++ deploy/03-deploy-dynamicsvg-nft.js
@@ -0,0 +1,39 @@
+const { network, ethers } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+const fs = require("fs")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    const chainId = network.config.chainId
+    let ethUsdPriceFeed
+
+    if (developmentChains.includes(network.name)) {
+        const EthUsdAggregator = await deployments.get("MockV3Aggregator")
+        ethUsdPriceFeed = EthUsdAggregator.address
+    } else {
+        ethUsdPriceFeed = networkConfig[chainId].ethUsdPriceFeed
+    }
+
+    const lowSvg = await fs.readFileSync("./images/dynamicNft/frown.svg", { encoding: "utf8" })
+    const highSvg = await fs.readFileSync("./images/dynamicNft/happy.svg", { encoding: "utf8" })
+
+    log("--------------------------------------------------------------")
+    const args = [ethUsdPriceFeed, lowSvg, highSvg]
+    const dynamicSvgNft = await deploy("DynamicSvgNft", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    log("-----------------------------------------------------------")
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        await verify(DynamicSvgNft.address, args)
+    }
+}
+
+module.exports.tags = ["all", "dynamicSvg", "main"]
