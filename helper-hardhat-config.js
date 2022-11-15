d616c0a7370d182898bfc06c4812850294285ee6
othaime-en <61498231+othaime-en@users.noreply.github.com>
2022-11-15 09:07:23 +0000

    remove comment

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -15,7 +15,7 @@ const networkConfig = {
         gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
         callbackGasLimit: "500000", // 500,000 gas
         mintFee: "10000000000000000", // 0.01 ETH
-        subscriptionId: "6107", // add your ID here!
+        subscriptionId: "6107",
     },
 }
 
